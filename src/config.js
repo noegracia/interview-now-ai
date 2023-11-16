2dfb4fa169db7aebc44ee401a412044be9fcd8da
Noé Gracia <noe.gracia@groupezekat.fr>
2023-11-16 19:23:27 +0100

    fix: untrack src/config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    user: "Noé Gracia AI Agent",
-    systemPrompt: "You are a candidate that is going to be asked questions by an interviewer.",
-    GPT_API_KEY: "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",
-    debugMode: false,
-  };
-  
-  export default config;
