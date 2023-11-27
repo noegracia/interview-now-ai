66cd689490be80770cfedd21f79bd595a16297ee
Noé Gracia <noe.gracia@groupezekat.fr>
2023-11-27 13:48:22 +0100

    fix: client side separated from server with proper public config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    user: "Noé Gracia AI Agent"
+  };
+  
+  export default config;
